f7395cb2dba9ec771758de769322bb490c52b956
Idan <idan945@gmail.com>
2016-01-28 01:03:55 +0200

    one day one day

--- Desktop/mission4/try/src/Run.java
+++ Desktop/mission4/try/src/Run.java
@@ -4,17 +4,23 @@ import java.util.ArrayList;
 
 public class Run {
 
+	volatile static Integer x = null;
+	
 	public static void main(String[] args) {
-	//	ArrayList<String> array= new ArrayList<>();
-	//	int a = 45;
-	//	array.add(String.valueOf(a));
-	//	System.out.println(array);
+	
+		if(x == null)
+			x = new Integer(3);
+		else 
+			System.out.println("blabla");
 		
-		String text = "1246";
-		if (text.matches("[0-9]+"))
-				System.out.println("only Numbers");
-		else
-			System.out.println("Not only Numbers");
+		System.out.println(x);
+		
+		try {
+			Thread.sleep(100000000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 }
